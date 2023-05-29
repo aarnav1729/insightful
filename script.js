05565a535ccf6a074ddfe86890f1761f53bd6952
aarnav singh <aarnavsingh836@gmail.com>
2023-05-29 23:53:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -39,49 +39,56 @@ function handleFormSubmit(event) {
 
 // Function to compare the followers and following lists
 function compareLists(followersList, followingList) {
-    // Split the lists into arrays
-    const followersArray = followersList.split('\n');
-    const followingArray = followingList.split('\n');
-    // New code to extract usernames from HTML
-    const innerSelector = "div._a6-p > div > div:nth-child(1) > a";
-    const listContainerSelector = "body > div > div > div > div._a705 > div._a706";
-    const l = [];
-    const a1 = Array.from(document.querySelector(listContainerSelector).children);
-      a1.forEach(el => {
-      l.push(el.querySelector(innerSelector).innerHTML);
-     });
+  // Split the lists into arrays
+  const followersArray = followersList.split('\n');
+  const followingArray = followingList.split('\n');
+
+  // Extract usernames from the HTML structure
+  const l = extractUsernamesFromHTML();
 
   // Log the extracted usernames
   console.log(l);
 
-  
-    // Clean up the arrays by removing any empty strings or whitespace
-    const cleanedFollowersArray = followersArray.filter(item => item.trim() !== '');
-    const cleanedFollowingArray = followingArray.filter(item => item.trim() !== '');
-  
-    // Find common and uncommon elements
-    const commonElements = cleanedFollowersArray.filter(item => cleanedFollowingArray.includes(item));
-    const uncommonElements = cleanedFollowingArray.filter(item => !cleanedFollowersArray.includes(item));
-  
-    // Display the comparison results
-    console.log('Followers List:', cleanedFollowersArray);
-    console.log('Following List:', cleanedFollowingArray);
-    console.log('Common Elements:', commonElements);
-    console.log('Uncommon Elements:', uncommonElements);
-  
-    // Generate the .txt file content
-    const txtContent = uncommonElements.join('\n');
-  
-    // Create a Blob from the .txt content
-    const blob = new Blob([txtContent], { type: 'text/plain' });
-  
-    // Create a download link for the .txt file
-    const downloadLink = document.createElement('a');
-    downloadLink.href = URL.createObjectURL(blob);
-    downloadLink.download = 'uncommon-elements.txt';
-    downloadLink.innerText = 'Download Uncommon Elements';
-  
-    // Append the download link to the placeholder element
-    const uncommonElementsLink = document.getElementById('uncommon-elements-link');
-    uncommonElementsLink.appendChild(downloadLink);
-  }  
+  // Clean up the arrays by removing any empty strings or whitespace
+  const cleanedFollowersArray = followersArray.filter(item => item.trim() !== '');
+  const cleanedFollowingArray = followingArray.filter(item => item.trim() !== '');
+
+  // Find common and uncommon elements
+  const commonElements = cleanedFollowersArray.filter(item => cleanedFollowingArray.includes(item));
+  const uncommonElements = cleanedFollowingArray.filter(item => !cleanedFollowersArray.includes(item));
+
+  // Display the comparison results
+  console.log('Followers List:', cleanedFollowersArray);
+  console.log('Following List:', cleanedFollowingArray);
+  console.log('Common Elements:', commonElements);
+  console.log('Uncommon Elements:', uncommonElements);
+
+  // Generate the .txt file content
+  const txtContent = uncommonElements.join('\n');
+
+  // Create a Blob from the .txt content
+  const blob = new Blob([txtContent], { type: 'text/plain' });
+
+  // Create a download link for the .txt file
+  const downloadLink = document.createElement('a');
+  downloadLink.href = URL.createObjectURL(blob);
+  downloadLink.download = 'uncommon-elements.txt';
+  downloadLink.innerText = 'Download Uncommon Elements';
+
+  // Append the download link to the placeholder element
+  const uncommonElementsLink = document.getElementById('uncommon-elements-link');
+  uncommonElementsLink.appendChild(downloadLink);
+}
+
+// Custom function to extract usernames from HTML structure
+function extractUsernamesFromHTML() {
+  const innerSelector = "div._a6-p > div > div:nth-child(1) > a";
+  const listContainerSelector = "body > div > div > div > div._a705 > div._a706";
+  const l = [];
+  const a1 = Array.from(document.querySelector(listContainerSelector).children);
+  a1.forEach(el => {
+    l.push(el.querySelector(innerSelector).innerHTML);
+  });
+
+  return l;
+}
