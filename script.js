ad19f0d894f9a1411b7998699a85dd52bbb83493
aarnav singh <aarnavsingh836@gmail.com>
2023-05-29 06:17:48 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,3 @@
-// Get the CTA button element
-const ctaButton = document.querySelector('.cta-button');
-
-// Add event listener to the CTA button
-ctaButton.addEventListener('click', handleCTAButtonClick);
-
-// Function to handle CTA button click event
-function handleCTAButtonClick(event) {
-  event.preventDefault();
-  
-  // Perform any necessary actions here, such as redirecting to the upload page
-  window.location.href = 'upload.html';
-}
-
 // Get the upload form element
 const uploadForm = document.getElementById('upload-form');
 
